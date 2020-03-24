7410df869de70a10617567b763a8e0f0a0ce480b
sunildasu1234 <sunildasu1234@gmail.com>
2020-03-24 02:33:37 +0100

    Add changes to the db
    Signed-off-by: sunildasu1234 <sunildasu1234@gmail.com>

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,12 +1,12 @@
-UPDATE DATABASE restapi;
-USE restapi;
-CREATE TABLE IF NOT EXISTS restapi.blog (
+UPDATE DATABASE test;
+USE test;
+CREATE TABLE IF NOT EXISTS test.blog (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(500)NOT NULL,
 content VARCHAR(5000)NOT NULL
 );
 
-INSERT INTO restapi.blog (title,content) values("Message","HelloNodeJSWorld");
+INSERT INTO test.blog (title,content) values("Message","HelloNodeJSWorld");
 
 commit;
 
